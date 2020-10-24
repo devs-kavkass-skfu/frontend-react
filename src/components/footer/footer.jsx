5322730cfa75b33a8a2eeec544475166e853d06f
Said1228 <said_guseynov_2015@mail.ru>
2020-10-24 15:45:52 +0300

    Fotter, images, summery

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,5 +1,13 @@
 import React from 'react' 
 import s from './footer.module.css'
+import logo from './../../assets/images/logo.svg'
+import vk from './../../assets/images/Vk.svg'
+import fb from './../../assets/images/FBook.svg'
+import ins from './../../assets/images/instogram.svg'
+import te from './../../assets/images/Telegram.svg'
+import tw from './../../assets/images/Tw.svg'
+import app from './../../assets/images/app.svg'
+import play from './../../assets/images/paly.png'
 
 
 
@@ -9,7 +17,39 @@ const Footer =(props)=>{
     return (
         <div className={s.all}>
             <div className={s.container}>
-                
+                <div className={s.info}>
+                    <ul>
+                        <li className = {s.head}>СКФУ работа   </li>
+                        <li>О сервисе</li>
+                        <li>Вакансии</li>
+                    </ul>
+                    <ul>
+                        <li className = {s.head}>Помощъ</li>
+                        <li>Для соискателя</li>
+                        <li>Для работадателя</li>
+                        <li>Служба поддержки</li>
+                    </ul>
+                    <ul>
+                        <li className = {s.head}>Документы</li>
+                        <li>Соглашение с пользователем</li>
+                        <li>Правила оказания услуг</li>
+                        <li>Наши партнеры</li>
+                    </ul>
+                </div>
+                <div className={s.link}>
+                    <div><img src={logo} alt=""/></div>
+                    <div>
+                        <img src={ins} alt=""/>
+                        <img src={fb} alt=""/>
+                        <img src={tw} alt=""/>
+                        <img src={vk} alt=""/>
+                        <img src={te} alt=""/>
+                    </div>
+                    <div>
+                        <img src={app} alt=""/>
+                        <img src={play} alt=""/>
+                    </div>
+                </div>
             </div>
            
             </div>
---
+++ src/pages/Sammery/summery.jsx
@@ -0,0 +1,15 @@
+import React from 'react' 
+import s from './summery.module.css'
+
+
+const Summery =(props)=>{
+    
+    return (
+        <div className={s.all}>
+            
+        </div>
+       
+    )
+}
+
+export default Summery  
